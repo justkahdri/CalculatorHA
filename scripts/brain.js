ff6419790a788c2546bcfc1a5c2b4df9fb4e5ba3
justkahdri <joaco.rm512@gmail.com>
2020-09-23 01:33:45 -0300

    renamed some vars

--- scripts/brain.js
+++ scripts/brain.js
@@ -1,88 +1,83 @@
-var remplazos = ['<sub>', '</sub>', '</sup>', '<sup>', '<br>'];
+const remplazos = ['<sub>', '</sub>', '</sup>', '<sup>', '<br>'];
 
 function Clase() {
-  var clear = [res, copiar, uni_estilos, corte_losa];
-  for (i in clear) {
-    clear[i].innerHTML = '';
-  }
-  var ejercicio = document.getElementById('ejercicio');
-  if (ejercicio.value == 'columna') {
-    Columna ();
-  }
-  else if (ejercicio.value == 'viga') {
-    Viga ();
-  }
-  else if (ejercicio.value == 'losa') {
-    Losa ();
-  }
-  else if (ejercicio.value == 'tabique') {
-    Tabique ();
-  }
-  else {
-    console.log('fallo la seleccion de clase');
-    alert('Ocurrio un error al iniciar el ejercicio');
-  }
+    const clear = [res, copiar, uni_estilos, corte_losa];
+    for (i in clear) {
+        clear[i].innerHTML = '';
+    }
+    let ejercicio = document.getElementById('ejercicio');
+    if (ejercicio.value == 'columna') {
+        Columna();
+    } else if (ejercicio.value == 'viga') {
+        Viga();
+    } else if (ejercicio.value == 'losa') {
+        Losa();
+    } else if (ejercicio.value == 'tabique') {
+        Tabique();
+    } else {
+        console.log('fallo la seleccion de clase');
+        alert('Ocurrio un error al iniciar el ejercicio');
+    }
 }
 
- function anadirASidebar(identficador, resultado, bases=false) {
-   var historial = document.getElementById('historial');
-   var ultimoEjercicio = document.createElement('details');
+function anadirASidebar(identficador, resultado, bases = false) {
+    const historial = document.getElementById('historial');
+    let ultimoEjercicio = document.createElement('details');
 
-   if (bases) {
-     historial.parentNode.removeChild(historial.parentNode.childNodes[2]);
-   }
+    if (bases) {
+        historial.parentNode.removeChild(historial.parentNode.childNodes[2]);
+    }
 
-   ultimoEjercicio.innerHTML = `
+    ultimoEjercicio.innerHTML = `
     <summary>${identficador}</summary>
     ${resultado.innerHTML}
     <span class='erase' onclick='erase_parent(this)'>Eliminar del historial</span>
    `;
 
-   historial.parentNode.insertBefore(ultimoEjercicio, historial.nextSibling);
+    historial.parentNode.insertBefore(ultimoEjercicio, historial.nextSibling);
 }
 
 function erase_parent(element) {
-  element.parentNode.remove();
+    element.parentNode.remove();
 }
 
 function copiarAlPortapapeles(id_elemento) {
 
-  var aux = document.createElement("input");
-  // Crea un campo de texto "oculto"
+    let aux = document.createElement("input");
+    // Crea un campo de texto "oculto"
 
-  aux.setAttribute("value", document.getElementById(id_elemento).innerHTML);
-  // Asigna el contenido del elemento especificado al valor del campo
+    aux.setAttribute("value", document.getElementById(id_elemento).innerHTML);
+    // Asigna el contenido del elemento especificado al valor del campo
 
-  //var str = aux.value;
+    //var str = aux.value;
 
-  for (i in remplazos) {
-    aux.value = aux.value.split(remplazos[i]).join('');
-  }
-  //Borra las etiquetas de HTML
+    for (i in remplazos) {
+        aux.value = aux.value.split(remplazos[i]).join('');
+    }
+    //Borra las etiquetas de HTML
 
-  document.body.appendChild(aux);
-  // Añade el campo a la página
+    document.body.appendChild(aux);
+    // Añade el campo a la página
 
-  aux.select();
-  // Selecciona el contenido del campo
-
-
-  document.execCommand("copy");
-  // Copia el texto seleccionado
+    aux.select();
+    // Selecciona el contenido del campo
 
 
-  document.body.removeChild(aux);
-  // Elimina el campo de la página
+    document.execCommand("copy");
+    // Copia el texto seleccionado
+
+
+    document.body.removeChild(aux);
+    // Elimina el campo de la página
 
 }
 
 function fullcost(string) {
-  var full = parseFloat((string).replace(',', '.'));
-  return full;
+    let full = parseFloat((string).replace(',', '.'));
+    return full;
 }
 
 
 
-var ejercicio;
 var inicio = document.getElementById('inicio');
 inicio.addEventListener('click', Clase);
--- scripts/columnas.js
+++ scripts/columnas.js
@@ -11,10 +11,10 @@ var copiar = document.getElementById('copiar');
 
 function Columna() {
 
-  var prin = document.getElementById('prin');
+    var prin = document.getElementById('prin');
 
-  console.log('llego a la columna');
-  prin.innerHTML = `
+    console.log('llego a la columna');
+    prin.innerHTML = `
     <form>
       <label for='numeracion'>
         <span>Numeraci&oacuten:</span>
@@ -45,80 +45,76 @@ function Columna() {
     </form>
   `;
 
-  var calcular = document.getElementById('calcular');
-  calcular.addEventListener('click', CComienzo);
+    var calcular = document.getElementById('calcular');
+    calcular.addEventListener('click', CComienzo);
 
 }
 
 
 function CComienzo() {
-  carga = document.getElementById('carga');
-  carga = fullcost(carga.value);
-  h = document.getElementById('H');
-  h = parseInt(h.value);
-  pisos = document.getElementById('pisos');
-  pisos = parseInt(1 + parseInt(pisos.value));
+    carga = document.getElementById('carga');
+    carga = fullcost(carga.value);
+    h = document.getElementById('H');
+    h = parseInt(h.value);
+    pisos = document.getElementById('pisos');
+    pisos = parseInt(1 + parseInt(pisos.value));
 
-  w = carga * 9 * pisos;
-  ag = (2.15 * w) / (0.85 * h/10);
-//Saco los decimales para evitar cualquier incoherencia de la division
+    w = carga * 9 * pisos;
+    ag = (2.15 * w) / (0.85 * h / 10);
+    //Saco los decimales para evitar cualquier incoherencia de la division
 
-  cx = (Math.sqrt(ag));
-  if (cx > 20) {
-    cx = Math.ceil(cx);
-    while (cx%5 != 0) { cx += 1; }
-    cy = 20;
-    while (((cx * cy) < ag) && (cy%5 != 0)) { cy += 1; }
-  }
+    cx = (Math.sqrt(ag));
+    if (cx > 20) {
+        cx = Math.ceil(cx);
+        while (cx % 5 != 0) { cx += 1; }
+        cy = 20;
+        while (((cx * cy) < ag) && (cy % 5 != 0)) { cy += 1; }
+    } else {
+        cx = 20;
+        cy = cx;
+    }
+    //Fuerzo el minimo de 20cm y que el resultado salte de 5 en 5
 
-  else {
-    cx = 20;
-    cy = cx;
-  }
-//Fuerzo el minimo de 20cm y que el resultado salte de 5 en 5
+    res.innerHTML = 'W= ' + w.toFixed(2) + 'KN <br>';
+    res.innerHTML += 'Ag= ' + ag.toFixed(2) + 'cm&sup2 <br>';
+    if (cx == cy) { res.innerHTML += 'C<sub>x</sub>= C<sub>y</sub>= ' + cx + 'cm <br>'; } else {
+        res.innerHTML += 'C<sub>x</sub>= ' + cx + 'cm <br>';
+        res.innerHTML += 'C<sub>y</sub>= ' + cy + 'cm <br>';
+    }
 
-  res.innerHTML = 'W= ' + w.toFixed(2) + 'KN <br>';
-  res.innerHTML += 'Ag= ' + ag.toFixed(2) + 'cm&sup2 <br>';
-  if (cx == cy) { res.innerHTML += 'C<sub>x</sub>= C<sub>y</sub>= ' + cx + 'cm <br>'; }
-  else {
-    res.innerHTML += 'C<sub>x</sub>= ' + cx + 'cm <br>';
-    res.innerHTML += 'C<sub>y</sub>= ' + cy + 'cm <br>';
-  }
+    numeracion = document.getElementById('numeracion').value;
+    anadirASidebar(numeracion, res);
 
-  numeracion = document.getElementById('numeracion').value;
-  anadirASidebar(numeracion, res);
-
-  copiar.innerHTML = '<button class="main-section__button" onclick=\'Bases(w)\'>Calcular Bases</button> <br>'; //Inicia la funcion de bases
-  copiar.innerHTML += '<button class="main-section__button" onclick=\'copiarAlPortapapeles("res")\'>Copiar Resultados</button>';
-//Imprimo los resultados y el boton para copiarlos dentro de los ultimos parrafos en HTML
+    copiar.innerHTML = '<button class="main-section__button" onclick=\'Bases(w)\'>Calcular Bases</button> <br>'; //Inicia la funcion de bases
+    copiar.innerHTML += '<button class="main-section__button" onclick=\'copiarAlPortapapeles("res")\'>Copiar Resultados</button>';
+    //Imprimo los resultados y el boton para copiarlos dentro de los ultimos parrafos en HTML
 }
 
 function Bases(w_columna) {
-  var w_base = w_columna * 1.1
-  var t_adm = 2
-  var ab = (w_base*100) / t_adm
-  var ax = Math.sqrt(ab)
-  if (ax > 100) {
-    ax = Math.ceil(ax);
-    while (ax%5 != 0) { ax += 1; }
-  }
-  else {ax = 100}
+    let w_base = w_columna * 1.1
+    const t_adm = 2
+    let ab = (w_base * 100) / t_adm
+    let ax = Math.sqrt(ab)
+    if (ax > 100) {
+        ax = Math.ceil(ax);
+        while (ax % 5 != 0) { ax += 1; }
+    } else { ax = 100 }
 
-//  var ay = ax
+    //  var ay = ax
 
-  res_bases = '<br>Wb= ' + w_base.toFixed(2) + 'KN <br>';
-  res_bases += 'Tadm= ' + t_adm + ' kg/cm&sup2 <br>';
-  res_bases += 'a<sub>x</sub>= a<sub>y</sub>= ' + ax + 'cm <br>';
+    res_bases = '<br>Wb= ' + w_base.toFixed(2) + 'KN <br>';
+    res_bases += 'Tadm= ' + t_adm + ' kg/cm&sup2 <br>';
+    res_bases += 'a<sub>x</sub>= a<sub>y</sub>= ' + ax + 'cm <br>';
 
-  res.innerHTML += res_bases;
-//  if (cx == cy) { res_bases.innerHTML += 'C<sub>x</sub>= C<sub>y</sub>= ' + cx + 'cm <br>'; }
-//  else {
-//    res_bases.innerHTML += 'C<sub>x</sub>= ' + cx + 'cm <br>';
-//    res_bases.innerHTML += 'C<sub>y</sub>= ' + cy + 'cm <br>';
-//  }
+    res.innerHTML += res_bases;
+    //  if (cx == cy) { res_bases.innerHTML += 'C<sub>x</sub>= C<sub>y</sub>= ' + cx + 'cm <br>'; }
+    //  else {
+    //    res_bases.innerHTML += 'C<sub>x</sub>= ' + cx + 'cm <br>';
+    //    res_bases.innerHTML += 'C<sub>y</sub>= ' + cy + 'cm <br>';
+    //  }
 
-  numeracion = document.getElementById('numeracion').value;
-  anadirASidebar(numeracion + ' (con Bases)', res, bases=true);
+    numeracion = document.getElementById('numeracion').value;
+    anadirASidebar(numeracion + ' (con Bases)', res, bases = true);
 
-  copiar.innerHTML = '<button class="main-section__button" onclick=\'copiarAlPortapapeles("res")\'>Copiar Resultados</button>';
+    copiar.innerHTML = '<button class="main-section__button" onclick=\'copiarAlPortapapeles("res")\'>Copiar Resultados</button>';
 }
